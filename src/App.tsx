6b8dde14b6143e959d5bd3af91dbf18093b28910
Levon <levon.s.avetisyan@gmail.com>
2025-01-06 13:10:05 +0400

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <i className="bi bi-globe me-3"></i>
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
